53341c12ba38230ea9268912b924facbaa53c91b
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-11-29 01:12:19 -0800

    edit Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="homeBackground0">
       <h1 className="homeName">RAOUL BUMBASI JR</h1>
       <h2 className="homeTitle0">WEBSITE DEVELOPER</h2>
+      <p className="homeTitle1">html/css, javascript, react, and adobe</p>
+      <Link to="/work" className="homeButton">
+        SEE MY WORK
+      </Link>
     </div>
   );
 };
