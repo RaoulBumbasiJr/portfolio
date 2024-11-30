e17c3fe06d24329f5e4c04156075b206258d459e
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-11-30 00:29:40 -0800

    Home edit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import Lightbulb from "../../assets/svg/lightbulb";
 
 const Home = () => {
   return (
@@ -14,8 +15,29 @@ const Home = () => {
         </Link>
       </div>
       <div className="homeSection1">
-        <div>
-          <h2></h2>
+        <div className="homeBlock">
+          <h2 className="homeHeader">Research</h2>
+          <Lightbulb className="homeSVG" />
+          <p className="homeParagraph">
+            I will research your product and create a website <br /> that
+            matches your market flawlessly.
+          </p>
+        </div>
+        <div className="homeBlock">
+          <h2 className="homeHeader">Research</h2>
+          <Lightbulb className="homeSVG" />
+          <p className="homeParagraph">
+            I will research your product and create a website <br /> that
+            matches your market flawlessly.
+          </p>
+        </div>
+        <div className="homeBlock">
+          <h2 className="homeHeader">Research</h2>
+          <Lightbulb className="homeSVG" />
+          <p className="homeParagraph">
+            I will research your product and create a website
+            <br /> that matches your market flawlessly.
+          </p>
         </div>
       </div>
     </div>
