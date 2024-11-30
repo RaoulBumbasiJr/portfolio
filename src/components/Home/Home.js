e50d2b2710af19b25ae241370ff607677e5a93f2
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-11-30 00:00:58 -0800

    Home edit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="homeBackground0">
-      <h1 className="homeName">RAOUL BUMBASI JR</h1>
-      <h2 className="homeTitle0">WEBSITE DEVELOPER</h2>
-      <p className="homeTitle1">html/css, javascript, react, and adobe</p>
-      <Link to="/work" className="homeButton">
-        SEE MY WORK
-      </Link>
+    <div>
+      <div className="homeBackground0">
+        <h1 className="homeName">RAOUL BUMBASI JR</h1>
+        <h2 className="homeTitle0">WEBSITE DEVELOPER</h2>
+        <p className="homeTitle1">html/css, javascript, react, and adobe</p>
+        <Link to="/work" className="homeButton">
+          SEE MY WORK
+        </Link>
+      </div>
+      <div className="homeSection1">
+        <div>
+          <h2></h2>
+        </div>
+      </div>
     </div>
   );
 };
