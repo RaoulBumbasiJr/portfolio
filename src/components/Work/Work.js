1d0eba16e53fdd0dbe6deeb5648e1e24a021bb1d
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-12-07 22:47:27 -0800

    Work.js edit

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -63,6 +63,24 @@ const Work = () => {
           <img src={skateapp} alt="skateapp" width="605" height="290" />
         </div>
       </div>
+      <div className="workColumn1">
+        <div className="workImageContainer">
+          <img src={skateapp} alt="skateapp" width="605" height="290" />
+        </div>
+        <div className="workTextContainer">
+          <h2 className="projectTitle">SKATEAPP.NET</h2>
+          <p className="projectDescription">
+            Website created for mobile app SkateApp. Made <br /> pages using
+            React Native. Front-end website <br /> and mobile development.
+          </p>
+          <div className="tags">
+            <Tag />
+            <t className="tagTitle">Website Development</t>
+            <Tag />
+            <t className="tagTitle">React Native</t>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
