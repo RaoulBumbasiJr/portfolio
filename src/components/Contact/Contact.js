686807963104f5f8fb8231ceb559f4d99b50bf13
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-12-08 23:00:24 -0800

    Contact.js edit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div>
-      <h1>Contact Me</h1>
-      <p>This is the Contact page where you can reach out to me.</p>
+      <div className="contactSection1">
+        <h1 className="contactHeading1">Let's have a conversation</h1>
+      </div>
+      <div>
+        <input className="name"></input>
+        <input className="email"></input>
+        <input className="budget"></input>
+        <input className="project description"></input>
+      </div>
     </div>
   );
 };
